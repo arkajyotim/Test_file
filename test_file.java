dd6b1d37e8b2b77862861b0c6c1eb3700192cd3e
arkajyotim <arkajyoti.mukhopadhyay2016@gmail.com>
2022-05-15 20:47:30 +0530

    1st edit

---
+++ test_file.java
@@ -0,0 +1,22 @@
+//java code to flip a number
+class Main {
+    public static void main(String[] args) {
+  
+      int num = 123456789, reversed = 0;
+      
+      System.out.println("Original Number: " + num);
+  
+      // run loop until num becomes 0
+      while(num != 0) {
+      
+        // get last digit from num
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+  
+        // remove the last digit from num
+        num /= 10;
+      }
+  
+      System.out.println("Reversed Number: " + reversed);
+    }
+  }
